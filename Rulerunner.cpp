aa28a09a4724e20fc0f61be75dc5bc8dd3d0c3f2
Chris Hartman <chris.m.hartman@gmail.com>
2018-04-16 10:55:18 -0800

    Flip and scale before drawing so DROP works properly

--- Rulerunner.cpp
+++ Rulerunner.cpp
@@ -1,28 +1,34 @@
 #include "Rulerunner.h"
+using std::string;
 #include "Cmd.h"
 
 #include <memory>
 #include <stdexcept>
 using std::logic_error;
 
+
 #ifdef _DEBUG
 #include <iostream>
 using namespace std;
 #endif
 
-void Rulerunner::handlerule(const std::string &rr, bool rulerev, bool ruleflip, double localscale) {
-    const auto & rule = _therules[rr]; //!!!eventually decorate with reverse and flip?
+void Rulerunner::handlerule(const string &rr, bool rulerev, bool ruleflip, double localscale) {
+    const auto & rule = _therules[rr]; //!!!eventually decorate with reverse and maybe flip?
     if (_rulestates.size() >= _maxdepth || _turtle.getscale() < _minscale) {
+        if(rulerev ^ ruleflip)
+            _turtle.flip();
+        _turtle.scaleby(localscale);
         _agraphic = _turtle.draw(rule);
+        _turtle.scaleby(1.0/localscale);
+        if(rulerev ^ ruleflip)
+            _turtle.flip();
     } else {
         bool willflip = rulerev ^ ruleflip;
-        _turtle.scaleby(localscale);
-        if (willflip)
-            _turtle.flip();
         bool currentlybw = _rulestates.empty() ? false : _rulestates.top().backwards; //!!! Should start with start rule on stack, then not check for empty() here
         _rulestates.push(Rulestate(&rule, currentlybw ^ rulerev, _turtle.getscale(), willflip));
-        double newscalefac = localscale * rule.cachedscalefac; //localscale is A@ 2 notation, cachedscalefac is A ? localscale 1/sqrt(2) notation
-        _turtle.scaleby(newscalefac);
+        if (willflip)
+            _turtle.flip();
+        _turtle.scaleby(localscale * rule.cachedscalefac);//localscale is A@ 2 notation, cachedscalefac is A ? localscale 1/sqrt(2) notation
     }
 }
 
@@ -43,6 +49,7 @@ std::shared_ptr<Graphic> Rulerunner::nextpoint() {
 
 void Rulerunner::makeapoint() {
     while (!_rulestates.empty() && !_agraphic.get()) {  //Go until we hit a turtle forward, or we're done
+        
         if (_rulestates.top().done()) {
             _turtle.setscale(_rulestates.top().oldscale);
             if (_rulestates.top().flipped)
@@ -57,7 +64,7 @@ void Rulerunner::makeapoint() {
 
 void Rulerunner::drawnextpoint() {
 #ifdef _DEBUG
-    if (agraphic == nullptr)
+    if (_agraphic == nullptr)
         throw logic_error("Called drawnextpoint() on a Rulerunner with no graphic ready\n");
 #endif
     _agraphic->draw();
