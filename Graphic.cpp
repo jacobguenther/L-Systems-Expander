1b56c8f6fb4657e63d758b82ee28a5aef2f530d1
Chris Hartman <chris.m.hartman@gmail.com>
2018-04-18 14:09:53 -0800

    Change draw() to pure virtual

--- Graphic.cpp
+++ Graphic.cpp
@@ -2,3 +2,11 @@
 
 bool Dropgraphic::haveapt = false;
 Point Dropgraphic::storedpt;  //!!! Surely there's a better way then static point CMH 4/8/18
+
+void Graphic::draw() const
+{
+    glBegin(GL_LINES);
+    glVertex2d(m.frompt.x, m.frompt.y);
+    glVertex2d(m.topt.x, m.topt.y);
+    glEnd();
+}
--- Graphic.h
+++ Graphic.h
@@ -16,12 +16,7 @@ class Graphic  //how do I make SURE this is an abstract base class?!!!
    public:
     explicit Graphic(const Motion &_m) : m(_m) {}
     Graphic()=default;
-    virtual void draw() {
-        glBegin(GL_LINES);
-        glVertex2d(m.frompt.x, m.frompt.y);
-        glVertex2d(m.topt.x, m.topt.y);
-        glEnd();
-    }
+    virtual void draw() const=0;
     virtual ~Graphic() = default;
     Graphic(const Graphic&) = delete;
     Graphic& operator=(const Graphic&) = delete;
@@ -47,8 +42,9 @@ class Dropgraphic : public Graphic {
     }
     friend class Rulerunner;
     friend class PopCommand;
-    void draw() override {
-        if (draws) Graphic::draw();
+    void draw() const override {
+        if (draws)
+            Graphic::draw();
     }
 
    private:
@@ -60,11 +56,16 @@ class Dropgraphic : public Graphic {
 class Linegraphic : public Graphic {
    public:
     explicit Linegraphic(const Motion &_m) : Graphic(_m) {}
+    void draw() const override   {
+        Graphic::draw();
+    }
+
 };
 
 class Invisgraphic : public Graphic {
    public:
-    void draw() override {}
+    void draw() const override
+    {}
 };
 
 class Rectgraphic : public Graphic {
