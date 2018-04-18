7fd85f0ec4aed6e8642c0cf5d96c4ed9865e403e
Chris Hartman <chris.m.hartman@gmail.com>
2018-04-18 09:35:50 -0800

    Add some parser tests

--- tests.cpp
+++ tests.cpp
@@ -4,4 +4,10 @@
 TEST_CASE( "Parsing expressions", "[Parser]" ) {
     auto p = Parser("0").parse();
     REQUIRE(p != nullptr);
+    REQUIRE(p->eval({})==Approx(0));
+    REQUIRE(Parser("4>2").parse()->eval());
+    REQUIRE(!bool(Parser("2>4").parse()->eval())); //!!! make eval have default parameter
+    REQUIRE(Parser("1?1:2").parse()->eval()==Approx(1));
+    REQUIRE(Parser("0?1:2").parse()->eval()==Approx(2));
+
 }
