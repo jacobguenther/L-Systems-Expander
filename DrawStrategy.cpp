21d833b9ef7d349f349081f3aa20ec715ec65fcf
Chris Hartman <chris.m.hartman@gmail.com>
2018-04-21 21:16:10 -0800

    Implement DropDrawStrategy

--- DrawStrategy.cpp
+++ DrawStrategy.cpp
@@ -14,10 +14,12 @@
 #include "Rulerunner.h"
 
 void LinesDrawStrategy::draw(Rulerunner& ruleRunner) {
-    if (ruleRunner._rulestates.top()._myRule.drawmethod==Rule::NONE)
+    if (ruleRunner._rulestates.top()._myRule.drawmethod == Rule::NONE)
         return;
     auto from = ruleRunner._turtle.getposition();
     ruleRunner._turtle.forward(1.0);
+    if (ruleRunner._rulestates.top()._myRule.drawmethod==Rule::INVIS)
+        return;
     auto to = ruleRunner._turtle.getposition();
     // !!! up to here is maybe a base class thing?
     glBegin(GL_LINES);
@@ -31,13 +33,24 @@ DropDrawStrategy::DropDrawStrategy(ParsenodePtr dropAngleExpression, ParsenodePt
 {}
 
 void DropDrawStrategy::draw(Rulerunner& ruleRunner) {
-    Motion todraw;
-//    todraw.frompt = storedpt;
-//    double dx = m.topt.x - m.frompt.x;
-//    double dy = m.topt.y - m.frompt.y;
-//    storedpt.x = m.frompt.x + dd * (cos(da) * dx - sin(da) * dy);
-//    storedpt.y = m.frompt.y + dd * (sin(da) * dx + cos(da) * dy);
-//    todraw.topt = storedpt;
-//    haveapt = true;
-//    m = todraw;
+    if (ruleRunner._rulestates.top()._myRule.drawmethod == Rule::NONE){
+        return;
+    }
+    auto from = ruleRunner._turtle.getposition();
+    ruleRunner._turtle.forward(1.0);
+    auto to = ruleRunner._turtle.getposition();
+    glBegin(GL_LINES);
+    glVertex2d(_lastDropped.x, _lastDropped.y);
+    double dx = to.x - from.x;
+    double dy = to.y - from.y;
+    std::cout << from.x << "," << from.y << " to " << to.x << "," << to.y << "\n";
+    std::cout << dx << " <-dx,dy-> " << dy << "\n";
+    auto dd = _dropDistanceExpression->eval(ruleRunner._context);
+    auto da = _dropAngleExpression->eval(ruleRunner._context)*ruleRunner._turtle.getflip()*DEG2RAD;//!!! cache these?
+    std::cout << dd << " <-dd,da-> " << da << "\n";
+    _lastDropped.x = from.x + dd * (cos(da) * dx - sin(da) * dy);
+    _lastDropped.y = from.y + dd * (sin(da) * dx + cos(da) * dy);
+    std::cout << _lastDropped.x << "," << _lastDropped.y << "\n";
+    glVertex2d(_lastDropped.x, _lastDropped.y);
+    glEnd();
 }
--- DrawStrategy.hpp
+++ DrawStrategy.hpp
@@ -42,6 +42,7 @@ public:
 private:
     ParsenodePtr _dropAngleExpression;
     ParsenodePtr _dropDistanceExpression;
+    Point _lastDropped;
 };
 
 
