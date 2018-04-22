761cf90e9727d65453e688fb49b5ba63574c2407
Chris Hartman <chris.m.hartman@gmail.com>
2018-04-22 12:58:35 -0800

    Pass rule by const &

--- DrawStrategy.hpp
+++ DrawStrategy.hpp
@@ -23,7 +23,7 @@ public:
     DrawStrategy(DrawStrategy&&) = delete;
     DrawStrategy& operator=(DrawStrategy&&) = delete;
 
-    virtual void draw(Rulerunner& ruleRunner, Rule &rule, bool ruleFlip, double atScale) =0;
+    virtual void draw(Rulerunner& ruleRunner, const Rule &rule, bool ruleFlip, double atScale) =0;
     virtual void start()=0;
     virtual void finish()=0;
 };
@@ -32,7 +32,7 @@ using DrawStrategyPtr = std::unique_ptr<DrawStrategy>;
 
 class LinesDrawStrategy : public DrawStrategy {
 public:
-    void draw(Rulerunner& ruleRunner, Rule &rule, bool ruleFlip, double atScale) override;
+    void draw(Rulerunner& ruleRunner, const Rule &rule, bool ruleFlip, double atScale) override;
     void start() override;
     void finish() override;
 };
@@ -42,7 +42,7 @@ public:
 class DropDrawStrategy : public DrawStrategy {
 public:
     DropDrawStrategy(ParsenodePtr dropAngleExpression, ParsenodePtr dropDistanceExpression);
-    void draw(Rulerunner& ruleRunner, Rule &rule, bool ruleFlip, double atScale) override;
+    void draw(Rulerunner& ruleRunner, const Rule &rule, bool ruleFlip, double atScale) override;
     void start() override;
     void finish() override;
 private:
