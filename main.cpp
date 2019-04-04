b9329356a133b403e0af6cb27b4ae75166b049d9
jacob guenther <jacobrwguenther@gmail.com>
2019-04-03 21:26:35 -0800

    Replace GlutFunctions with GLFWwrapper.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include "Cmd.h"
 #include "Context.h"
-#include "GlutFunctions.hpp"
+#include "GLFWwrapper.hpp"
 #include "Lsystem.h"
 #include <algorithm>
 #include <iostream>
@@ -20,9 +20,9 @@ int main(int argc, char **argv) {
         cout << "Use [ and ] to change threshold for dynamic level drawing." << endl;
         cout << "Use u and v to change parameter for parameterized fractals." << endl;
 
-        readtheconfigfile();
-        runGlut(argc,argv);
-        
+        WindowCreateData createData;
+        GLFWwrapper wrapper(createData);
+        wrapper.run();
         return 0;
     } catch (std::exception &error) {
         std::cerr << error.what() << endl;
